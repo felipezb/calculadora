76ec0eab5bcd36b42e186dcb58c59452d44d5c7c
felipezb <felipezanella18@gmail.com>
2025-01-16 21:32:47 -0300

    correções

---
+++ src/calculadora.js
@@ -0,0 +1,14 @@
+function soma() {
+    const num1 = parseFloat(document.getElementById('numero1').value);
+    const num2 = parseFloat(document.getElementById('numero2').value);
+    const resultado = num1 + num2;
+    alert(`Resultado da soma: ${resultado}`); // Exibe o resultado em uma mensagem pop-up
+  }
+  
+  function subtrai() {
+    const num1 = parseFloat(document.getElementById('numero1').value);
+    const num2 = parseFloat(document.getElementById('numero2').value);
+    const resultado = num1 - num2;
+    alert(`Resultado da subtração: ${resultado}`); // Exibe o resultado em uma mensagem pop-up
+  }
+  
